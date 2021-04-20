074cc2813b6deef8b8f4b2a408be9213f78b5785
Harvey Cortes <harveydavid14@gmail.com>
2021-04-19 19:00:09 -0500

    removed tesNG dependencies and added Cucumber

--- src/main/java/PageObjects/LandingPage.java
+++ src/main/java/PageObjects/LandingPage.java
@@ -3,7 +3,6 @@ package PageObjects;
 import PageComponents.Footer;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.Test;
 import utils.base;
 
 
--- src/test/java/dummyTest.java
+++ src/test/java/dummyTest.java
@@ -2,10 +2,6 @@ import PageComponents.Footer;
 import PageObjects.LandingPage;
 import com.aventstack.extentreports.ExtentTest;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 import utils.base;
 
 import java.io.IOException;
@@ -19,25 +15,5 @@ public class dummyTest extends base {
 
     public WebDriver driver;
     LandingPage landingPage;
-
-    @BeforeTest
-    public void setUpDriver() throws IOException {
-        this.driver = initializeDriver();
-        landingPage = new LandingPage(driver);
-        logger.info("Driver initialized");
-    }
-
-    @Test
-    public void basePageNavigation () throws IOException {
-        driver.get("http://automationpractice.com/index.php");
-        logger.info("Navigating to the landing page");
-        landingPage.getFooterNavigation().getLinkTextWomen().click();
-        Assert.assertTrue(false);
-    }
-
-    @AfterTest
-    public void quitDriver(){
-        driver.quit();
-    }
 
 }
